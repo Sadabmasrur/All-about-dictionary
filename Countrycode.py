149d9a786221dd45943b082cea0a506d9a524c2c
Sadab Masrur <sadabmasrur08@gmail.com>
2025-02-22 20:11:23 +0600

    acp

---
+++ Countrycode.py
@@ -0,0 +1,14 @@
+country_code= {
+    'England':'44',
+    'Australia':'25',
+    'Bangladesh':'880'
+}
+
+print("Country code for England-")
+print(country_code.get('England','Not found'))
+
+print("Country code for Australia-")
+print(country_code.get('Australia','Not found'))
+
+print("Country code for Riponmialand-")
+print(country_code.get('Riponmialand','Not found'))
